4f45e3cdfe98779ee95d1b191632410395f5bd72
Leo <leo0208@126.com>
2024-11-14 05:06:24 +0800

    Site updated: 2024-11-14 05:06:24

--- js/redirect.js
+++ js/redirect.js
@@ -1,11 +1,28 @@
-var currentUrl = window.location.href;
+const currentUrl = window.location.href;
 
-// 检查是否包含 "http://www.xn--w8yz0bb74a.com" 但不包含 "http://www.xn--w8yz0bb74a.com/post/keyanlun.html"
-if (currentUrl.includes("http://www.xn--w8yz0bb74a.com") && !currentUrl.includes("http://www.xn--w8yz0bb74a.com/post/keyanlun.html")) {
-    
-    // 定义重定向的目标 URL
-    var redirectTo = "http://www.xn--w8yz0bb74a.com/post/keyanlun.html";
- 
-    // 进行重定向
-    window.location.replace(redirectTo);
+// 定义重定向规则
+const redirects = [
+    {
+        source: ["www.xn--w8yz0bb74a.com", "www.keyanlun.com"],
+        target: "http://www.xn--w8yz0bb74a.com/post/keyanlun.html"
+    },
+    // 可以在这里添加更多的重定向规则
+    // {
+    //     source: ["example.com", "another-example.com"],
+    //     target: "http://example.com/target-page"
+    // }
+];
+
+// 检查是否需要重定向
+function shouldRedirect(url, redirect) {
+    const urlObj = new URL(url);
+    return redirect.source.some(domain => urlObj.hostname === domain) && url !== redirect.target;
+}
+
+// 执行重定向
+for (const redirect of redirects) {
+    if (shouldRedirect(currentUrl, redirect)) {
+        window.location.replace(redirect.target);
+        break;
+    }
 }
