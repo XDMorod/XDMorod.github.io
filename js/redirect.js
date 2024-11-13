4dbb5f33b95c010f6ed00972ca04a02ba3f7c1f3
Leo <leo0208@126.com>
2024-11-14 05:54:38 +0800

    Site updated: 2024-11-14 05:54:37

--- js/redirect.js
+++ js/redirect.js
@@ -1,7 +1,9 @@
 var currentUrl = window.location.href;
 
 // 检查是否包含 "http://www.xn--w8yz0bb74a.com" 或 "http://www.keyanlun.com" 但不包含 "http://www.xn--w8yz0bb74a.com/post/keyanlun.html"
-if ((currentUrl.includes("http://www.xn--w8yz0bb74a.com") || currentUrl.includes("http://www.keyanlun.com")) && !currentUrl.includes("http://www.xn--w8yz0bb74a.com/post/keyanlun.html")) {
+// if ((currentUrl.includes("http://www.xn--w8yz0bb74a.com") || currentUrl.includes("http://www.keyanlun.com")) && !currentUrl.includes("http://www.xn--w8yz0bb74a.com/post/keyanlun.html")) 
+if (currentUrl.includes("http://www.xn--w8yz0bb74a.com") )  && !currentUrl.includes("http://www.xn--w8yz0bb74a.com/post/keyanlun.html"))
+    {
     
     // 定义重定向的目标 URL
     var redirectTo = "http://www.xn--w8yz0bb74a.com/post/keyanlun.html";
