f6d87441f1cf7cf9c8e0e8a2359e1d8af474003d
Leo <leo0208@126.com>
2024-12-06 09:07:08 +0800

    Site updated: 2024-12-06 09:07:06

---
+++ js/onlinecounter.js
@@ -0,0 +1,38 @@
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?3bd5bc8cc93523597126a509970be181";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+
+
+// (
+//     function () {
+    
+          
+//     getOnlineUser()
+    
+          
+//     function getOnlineUser() {
+//         // 移除之前的 online-counter
+//         let oldScript = document.getElementById("online-counter");
+//         if (oldScript) {
+//             oldScript.remove();
+//         }
+//         //create script
+//         let script = document.createElement("script");
+//         script.src = "https://time-counter.onmicrosoft.cn/counter.js";
+//         script.async = true;
+//         script.id = "online-counter";
+//         script.setAttribute("interval", 240);
+//         script.setAttribute("api", "https://time-counter.onmicrosoft.cn/counter");
+//         script.setAttribute("room", "{Room ID}");
+//         document.head.appendChild(script);
+//     }
+    
+          
+//     }
+//     )()
